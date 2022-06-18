9deeadd230f4438676e083254405cb4f622422bf
ksenya31 <kseniakalinnikova268@gmail.com>
2022-06-18 15:23:21 +0300

    added

---
+++ src/test/java/StatServiceTest.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StatServiceTest {
+    @Test
+    public void shouldSum() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAverage() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.average(sales);
+        long expected = 15;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMaxSalesMonth() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.maxSalesMonth(sales);
+        long expected = 8;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMinSalesMonth() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.minSalesMonth(sales);
+        long expected = 9;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAboveAverage() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.aboveAverage(sales);
+        long expected = 5;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldBelowAverage() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.belowAverage(sales);
+        long expected = 5;
+        assertEquals(expected, actual);
+    }
+}
+
+
